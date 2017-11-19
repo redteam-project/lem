c6db24694cf4db7306aa9262bb89363d159af47f
Kenneth D. Evensen <kevensen@redhat.com>
2017-11-19 11:20:17 -0500

    Release for 0.2.2

--- elem/elem_configuration.py
+++ elem/elem_configuration.py
@@ -15,7 +15,17 @@ class ElemConfiguration(object):
         else:
             self.path = os.path.join(os.path.expanduser("~"), '.elem')
 
+        # In Python 2.6, the virtualenv cache is funky. 
+        if not os.path.isdir(self.path):
+            os.makedirs(self.path)
+
         self.file = os.path.join(self.path, "elem.conf")
+
+        dir_path = os.path.dirname(os.path.realpath(__file__))
+        source_config = os.path.join(dir_path, 'config', 'elem.conf')
+        if not os.path.isfile(self.file):
+            shutil.copy(source_config, self.file)
+        #End workaround for funky
 
     def read_config(self):
         config = ConfigParser.ConfigParser()
