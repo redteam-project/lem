ab73e2f9eb96e36a96a35cd57fa923ed61c0f5d2
Jason Callaway <jason@jasoncallaway.com>
2019-03-13 12:44:58 -0400

    missed some elems in test lem configuration

--- tests/test_lem_configuration.py
+++ tests/test_lem_configuration.py
@@ -1,15 +1,15 @@
 import unittest
 import os
-from lem import ElemConfiguration
+from lem import LemConfiguration
 
-class TestElemConfiguration(unittest.TestCase):
+class TestLemConfiguration(unittest.TestCase):
 
     def setUp(self):
-        os.environ["ELEMCONFPATH"] = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'test_config')
-        self.config = ElemConfiguration()
+        os.environ["LEMCONFPATH"] = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'test_config')
+        self.config = LemConfiguration()
 
     def test_get_config(self):
-        self.assertEqual(os.getenv("ELEMCONFPATH"), self.config.path)
+        self.assertEqual(os.getenv("LEMCONFPATH"), self.config.path)
 
     def tearDown(self):
         os.remove(self.config.file)
